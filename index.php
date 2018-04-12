296df017a8b7ba6cc938f72335cfb3e304ef029f
Antoine Angoulvant <antoine.angoulvant@yahoo.fr>
2018-04-12 13:38:52 +0200

    Indentation

--- index.php
+++ index.php
@@ -41,21 +41,21 @@
 
             ?>
 
-            </div>
-            <!-- /.container-fluid-->
-            <!-- /.content-wrapper-->
-            <footer class="sticky-footer">
-                <div class="container">
-                    <div class="text-center">
-                        <small>Réalisé par Antoine Angoulvant - David Luong - Guillaume Rougeau - Jérôme Mialon</small>
-                    </div>
+        </div>
+        <!-- /.container-fluid-->
+        <!-- /.content-wrapper-->
+        <footer class="sticky-footer">
+            <div class="container">
+                <div class="text-center">
+                    <small>Réalisé par Antoine Angoulvant - David Luong - Guillaume Rougeau - Jérôme Mialon</small>
                 </div>
-            </footer>
-            <!-- Scroll to Top Button-->
-            <a class="scroll-to-top rounded" href="#page-top">
-                <i class="fa fa-angle-up"></i>
-            </a>
-            <!-- Logout Modal-->
+            </div>
+        </footer>
+        <!-- Scroll to Top Button-->
+        <a class="scroll-to-top rounded" href="#page-top">
+            <i class="fa fa-angle-up"></i>
+        </a>
+        <!-- Logout Modal-->
         <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
