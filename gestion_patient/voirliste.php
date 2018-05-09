8196895e0a6dcbc97a7ec0008e2275577c890f13
David <C00221536@itcarlow.ie>
2018-05-09 19:47:52 +0200

    Add modal pour Ajouter Tache via liste patient

--- gestion_patient/voirliste.php
+++ gestion_patient/voirliste.php
@@ -26,10 +26,9 @@ $resultat = $req->fetch();
     </div>
     <form method="post" action="index.php?page=voirliste&id=<?php echo $_GET['id']; ?>">
 
-    </form>
-        <?php if($envoye) echo '<div class="alert alert-success">Modifications enregistrées !</div>'; ?>
-        <button type="submit" class="btn btn-primary center-block" style="margin-bottom: 30px;">Enregistrer</button>
-    </form>
+        <button type="button" class="btn btn-primary center-block" data-toggle="modal" data-target="#ajouter">
+            Ajouter tache
+        </button>
 </div>
 <div class="card-body">
     <div class="table-responsive">
@@ -52,7 +51,7 @@ $resultat = $req->fetch();
             </tfoot>
             <tbody>
             <?php
-            $idPat = $_GET['id'];;
+            $idPat = $_GET['id'];
             $rep = $bdd->query("SELECT * FROM liste Where idPatient=$idPat");
             while ($donnees = $rep->fetch()) {
                 echo '<tr>';
@@ -71,6 +70,43 @@ $resultat = $req->fetch();
                     <input type="hidden" name="id" value="<?php echo $donnees['idTache']; ?>">
                     <button type="delete" name="delete" class="btn btn-danger">Supprimer</button>
                 </form>
+                    <div class="modal fade" id="ajouter" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    <form method="post" action="">
+                                        <input type="hidden" name="id" value="<?php echo $idPat; ?>">
+                                        <form method="post" action="">
+                                            <div class="row margininscription">
+                                                <div class="">
+                                                    <div class="form-group">
+                                                        <label for="genre">Nom tache<span class="obligatoire">*</span> :</label>
+                                                        <input type="text" class="form-control" id="tache" placeholder="Saisissez la tache" name="tache">
+                                                    </div>
+                                                </div>
+                                                <div class="">
+                                                    <div class="form-group">
+                                                        <label for="prenom">Prénom<span class="obligatoire">*</span> :</label>
+                                                        <input type="text" class="form-control" id="commentaire" placeholder="Saisissez le commentaire" name="commentaire">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <button type="ajouter" name="ajouter" class="btn btn-danger">Ajouter</button>
+                                        </form>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-primary">Save changes</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 <?php
             }
             $rep->closeCursor();
